35cc5a2d6f1f814a92482b4bac32b79075c9af2e
tamakiii <tamakiii@users.noreply.github.com>
2020-10-17 14:08:15 +0900

    init vim.mk

---
+++ vim.mk
@@ -0,0 +1,19 @@
+.PHONY: help setup dependencies install clean
+
+help:
+	@cat $(firstword $(MAKEFILE_LIST))
+
+setup: \
+	dependencies
+
+dependencies:
+	@type curl > /dev/null
+
+install: \
+	~/.vim/autoload/plug.vim
+
+~/.vim/autoload/plug.vim:
+	curl -fLo $@ --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+clean:
+	rm -rf ~/.vim/autoload/plug.vim
